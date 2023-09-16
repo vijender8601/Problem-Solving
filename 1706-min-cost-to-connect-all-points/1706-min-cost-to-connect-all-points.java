647533dc86ecef1561acca67bf051084f397d900
vijender8601 <93083156+vijender8601@users.noreply.github.com>
2023-09-16 13:51:15 +0530

    Time: 541 ms (23.24%), Space: 60.4 MB (57.25%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -6,8 +6,8 @@ public class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         boolean[] visited = new boolean[n];
-        HashMap<Integer, Integer> heap_dict = new HashMap<>();
-        heap_dict.put(0, 0);
+        // HashMap<Integer, Integer> heap_dict = new HashMap<>();
+        // heap_dict.put(0, 0);
         
         PriorityQueue<int[]> min_heap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
         min_heap.add(new int[]{0, 0});
@@ -18,7 +18,7 @@ public class Solution {
             int[] top = min_heap.poll();
             int w = top[0], u = top[1];
             
-            if (visited[u] || heap_dict.getOrDefault(u, Integer.MAX_VALUE) < w) continue;
+            if (visited[u] ) continue;
             
             visited[u] = true;
             mst_weight += w;
@@ -26,10 +26,10 @@ public class Solution {
             for (int v = 0; v < n; ++v) {
                 if (!visited[v]) {
                     int new_distance = manhattan_distance(points[u], points[v]);
-                    if (new_distance < heap_dict.getOrDefault(v, Integer.MAX_VALUE)) {
-                        heap_dict.put(v, new_distance);
+                    // if (new_distance < heap_dict.getOrDefault(v, Integer.MAX_VALUE)) {
+                        // heap_dict.put(v, new_distance);
                         min_heap.add(new int[]{new_distance, v});
-                    }
+                    // }
                 }
             }
         }
