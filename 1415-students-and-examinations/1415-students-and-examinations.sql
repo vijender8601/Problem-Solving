28f7a3ae0bd95a04547bc95bc9f51239c82bc18e
vijender8601 <93083156+vijender8601@users.noreply.github.com>
2023-09-19 18:50:32 +0530

    Time: 2249 ms (36.04%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -4,3 +4,5 @@ ON st.student_id = ex.student_id AND sc.subject_name = ex.subject_name
 GROUP BY st.student_id,st.student_name,sc.subject_name
 ORDER BY st.student_id, sc.subject_name
 ;
+
+# cross join of 3 table
